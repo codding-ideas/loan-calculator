906d6bf9151f2cf6937a8c45ea198d97becc3561
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-06-23 14:48:22 +1000

    Built form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <form>
+        <table>
+           <caption>Simple Interest Calculator</caption>
+           <tr>
+             <td><input type="number" /></td>
+           </tr>
+
+          
+        </table>
+
+      </form>
     </div>
   );
 }
