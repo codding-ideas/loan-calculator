f3484fd155b03ef8f4863ad954bfea3afb9ddb23
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-06-23 13:11:27 +0800

    Form moved to it component

---
+++ .history/src/components/Form_20190623130520.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Form = () => {
+    return(
+        <div>
+            <form>
+                <input type = 'text'/>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
---
+++ .history/src/components/Form_20190623130757.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+const Form = () => {
+    return(
+        <div>
+           <form>
+        <table>
+           <caption>Simple Interest Calculator</caption>
+           <tr>
+             <td><input type="number" /></td>
+           </tr>
+
+          
+        </table>
+
+      </form>
+        </div>
+    )
+}
+
+export default Form;
